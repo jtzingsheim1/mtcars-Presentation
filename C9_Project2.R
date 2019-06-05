8aaa94aecdb00b92c56fb8fd36ce84c187181106
jtzingsheim1 <43869157+jtzingsheim1@users.noreply.github.com>
2019-06-05 18:35:58 -0500

    Created script file and updated overview section

---
+++ C9_Project2.R
@@ -0,0 +1,26 @@
+# Coursera Data Science Specialization Course 9 Project 2 Script----------------
+# Plot of mtcars Data
+
+
+# The purpose of this script is to complete the basic requirements behind the
+# project 2 peer-graded assignment which is part of the Developing Data Products
+# course from Johns Hopkins University within the Data Science Specialization on
+# Coursera.
+#
+# The instructions say:
+# "Create a web page presentation using R Markdown that features a plot created
+# with Plotly. Host your webpage on either GitHub Pages, RPubs, or NeoCities.
+# Your webpage must contain the date that you created the document, and it must
+# contain a plot created with Plotly. We would love to see you show off your
+# creativity!"
+#
+# This project will meet the objective by creating a plot of the mtcars data
+# which is the only input for the script.
+
+
+library(datasets)  # For the mtcars data
+library(tidyverse)
+library(plotly)
+
+
+# Part 1) Loading and preprocessing the data------------------------------------
